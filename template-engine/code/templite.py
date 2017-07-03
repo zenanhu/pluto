77eecb7a809a7b4f56d70e6d7e09deb2c7e0188b
Zenan <zenan2048@gmail.com>
2017-07-03 18:28:06 +0800

    add template engine

---
+++ template-engine/code/templite.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+
+class CodeBuilder(object):
+    INDENT_STEP = 4
+
+    def __init__(self, indent=0):
+        self.code = []
+        self.indent_level = indent
+
+    def add_line(self, line):
+        self.code.extend([" " * self.indent_level, line, "\n"])
+
+    def indent(self):
+        self.indent_level += self.INDENT_STEP
+
+    def dedent(self):
+        self.indent_level -= self.INDENT_STEP
+
+    def add_section(self):
+        section = CodeBuilder(self.indent_level)
+        self.code.append(section)
+        return section
+
+    def __str__(self):
+        return "".join(str(c) for c in self.code)
+
+    def get_globals(self):
+        assert self.indent_level == 0
+        python_source = str(self)
+        global_namespace = {}
+        exec(python_source, global_namespace)
+        return global_namespace
+
+
+class Templite(object):
+
+    def __init__(self, text, *contexts):
+        self.context = {}
+        for context in contexts:
+            self.context.update(context)
+
+        self.all_vars = set()
+        self.loop_vars = set()
+
+        code = Codebuilder()
+        code.add_line("def render_function(context, do_dots):")
+        code.indent()
+        vars_code = code.add_section()
+        code.add_line("result = []")
+        code.add_line("append_result = result.append")
+        code.add_line("extend_result = result.extend")
+        code.add_line("to_str = str")
+
+
+
