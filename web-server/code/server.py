4647efc3f6d24f08461361dee2aeeb4dcabe8c91
Zenan <zenan2048@gmail.com>
2017-07-11 13:30:20 +0800

    Finish web-server

--- web-server/code/server.py
+++ web-server/code/server.py
@@ -9,7 +9,47 @@ class ServerException(Exception):
     pass
 
 
-class CaseNoFile(object):
+class BaseCase(object):
+    Listing_Page = '''
+    <html>
+    <body>
+        <ul>
+            {0}
+        </ul>
+    </body>
+    </html>
+    '''
+
+    def handle_file(self, handler, path):
+        try:
+            with open(path, 'rb') as reader:
+                content = reader.read()
+                handler.send_content(content)
+        except IOError as msg:
+            msg = "'{0}' can't be read: {1}".format(path, msg)
+            handler.handle_error(msg)
+
+    def list_dir(self, handler, full_path):
+        try:
+            entries = os.listdir(full_path)
+            bullets = ["<li>{0}<li>".format(e) for e in entries if not e.startswith('.')]
+            page = self.Listing_Page.format('\n'.join(bullets))
+            handler.send_content(page)
+        except OSError as msg:
+            msg = "'{0}' cannot be listed: {1}".format(full_path, msg)
+            handler.handle_error(msg)
+
+    def index_path(self, handler):
+        return os.path.join(handler.full_path, 'index.html')
+
+    def test(self, handler):
+        assert False, 'Not implemented.'
+
+    def act(self, handler):
+        assert False, 'Not implemented.'
+
+
+class CaseNoFile(BaseCase):
     def test(self, handler):
         return not os.path.exists(handler.full_path)
 
@@ -17,37 +57,44 @@ class CaseNoFile(object):
         raise ServerException("'{0}' not exist".format(handler.path))
 
 
-class CaseExistingFile(object):
+class CaseCGIFile(BaseCase):
+    def test(self, handler):
+        return os.path.isfile(handler.full_path) and handler.full_path.endswith('.py')
+
+    def act(self, handler):
+        cmd = 'python ' + handler.full_path
+        child_stdin, child_stdout = os.popen2(cmd)
+        child_stdin.close()
+        data = child_stdout.read()
+        child_stdout.close()
+        handler.send_content(data)
+
+
+class CaseExistingFile(BaseCase):
     def test(self, handler):
         return os.path.isfile(handler.full_path)
 
     def act(self, handler):
-        handler.handle_file(handler.full_path)
+        self.handle_file(handler, handler.full_path)
 
 
-class CaseDirectoryIndexFile(object):
-    def index_path(self, handler):
-        return os.path.join(handler.full_path, 'index.html')
-
+class CaseDirectoryIndexFile(BaseCase):
     def test(self, handler):
         return os.path.isdir(handler.full_path) and os.path.isfile(self.index_path(handler))
 
     def act(self, handler):
-        handler.handle_file(self.index_path(handler))
+        self.handle_file(handler, self.index_path(handler))
 
 
-class CaseDirectoryNoIndexFile(object):
-    def index_path(self, handler):
-        return os.path.join(handler.full_path, 'index.html')
-
+class CaseDirectoryNoIndexFile(BaseCase):
     def test(self, handler):
         return os.path.isdir(handler.full_path) and not os.path.isfile(self.index_path(handler))
 
     def act(self, handler):
-        handler.list_dir(handler.full_path)
+        self.list_dir(handler, handler.full_path)
 
 
-class CaseAlwaysFail(object):
+class CaseAlwaysFail(BaseCase):
     def test(self, handler):
         return True
 
@@ -56,21 +103,6 @@ class CaseAlwaysFail(object):
 
 
 class RequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
-    Page = '''\
-<html>
-<body>
-    <table>
-        <tr>  <td>Header</td>         <td>Value</td>          </tr>
-        <tr>  <td>Date and time</td>  <td>{date_time}</td>    </tr>
-        <tr>  <td>Client host</td>    <td>{client_host}</td>  </tr>
-        <tr>  <td>Client port</td>    <td>{client_port}s</td> </tr>
-        <tr>  <td>Command</td>        <td>{command}</td>      </tr>
-        <tr>  <td>Path</td>           <td>{path}</td>         </tr>
-    </table>
-</body>
-</html>
-'''
-
     Error_Page = """\
 <html>
 <body>
@@ -80,17 +112,9 @@ class RequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
 </html>
 """
 
-    Listing_Page = '''
-<html>
-<body>
-    <ul>
-        {0}
-    </ul>
-</body>
-</html>
-'''
     Cases = [
         CaseNoFile(),
+        CaseCGIFile(),
         CaseExistingFile(),
         CaseDirectoryIndexFile(),
         CaseDirectoryNoIndexFile(),
@@ -108,41 +132,11 @@ class RequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
         except Exception as msg:
             self.handle_error(msg)
 
-    def handle_file(self, path):
-        try:
-            with open(path, 'rb') as reader:
-                content = reader.read()
-                self.send_page(content)
-        except IOError as msg:
-            msg = "'{0}' can't be read: {1}".format(self.path, msg)
-            self.handle_error(msg)
-
     def handle_error(self, msg):
         content = self.Error_Page.format(path=self.path, msg=msg)
-        self.send_page(content, status=404)
+        self.send_content(content, status=404)
 
-    def list_dir(self, full_path):
-        try:
-            entries = os.listdir(full_path)
-            bullets = ["<li>{0}<li>".format(e) for e in entries if not e.startswith('.')]
-            page = self.Listing_Page.format('\n'.join(bullets))
-            self.send_page(page)
-        except OSError as msg:
-            msg = "'{0}' cannot be listed: {1}".format(self.path, msg)
-            self.handle_error(msg)
-
-    def create_page(self):
-        values = {
-            'date_time': self.date_time_string(),
-            'client_host': self.client_address[0],
-            'client_port': self.client_address[1],
-            'command': self.command,
-            'path': self.path,
-        }
-        page = self.Page.format(**values)
-        return page
-
-    def send_page(self, page, status=200):
+    def send_content(self, page, status=200):
         self.send_response(status)
         self.send_header("Content-Type", "text/html")
         self.send_header("Content-Length", str(len(page)))
