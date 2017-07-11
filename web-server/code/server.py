bd73608d2037a72b8049e685392a678660d0c248
Zenan <zenan2048@gmail.com>
2017-07-11 12:11:30 +0800

    add handlers

--- web-server/code/server.py
+++ web-server/code/server.py
@@ -1,23 +1,107 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+import os
 import BaseHTTPServer
+
+
+class ServerException(Exception):
+    pass
+
+
+class CaseNoFile(object):
+    def test(self, handler):
+        return not os.path.exists(handler.full_path)
+
+    def act(self, handler):
+        raise ServerException("'{0}' not exist".format(handler.path))
+
+
+class CaseExistingFile(object):
+    def test(self, handler):
+        return os.path.isfile(handler.full_path)
+
+    def act(self, handler):
+        handler.handle_file(handler.full_path)
+
+
+class CaseAlwaysFail(object):
+    def test(self, handler):
+        return True
+
+    def act(self, handler):
+        raise ServerException("Unknown object '{0}'".format(handler.path))
 
 
 class RequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
     Page = '''\
-            <html>
-                <body>
-                    <p>Hello, web!</p>
-                </body>
-            </html>'''
+<html>
+<body>
+    <table>
+        <tr>  <td>Header</td>         <td>Value</td>          </tr>
+        <tr>  <td>Date and time</td>  <td>{date_time}</td>    </tr>
+        <tr>  <td>Client host</td>    <td>{client_host}</td>  </tr>
+        <tr>  <td>Client port</td>    <td>{client_port}s</td> </tr>
+        <tr>  <td>Command</td>        <td>{command}</td>      </tr>
+        <tr>  <td>Path</td>           <td>{path}</td>         </tr>
+    </table>
+</body>
+</html>
+'''
+
+    Error_Page = """\
+<html>
+<body>
+    <h1>Error accessing {path}</h1>
+    <p>{msg}</p>
+</body>
+</html>
+"""
+
+    Cases = [CaseNoFile, CaseExistingFile, CaseAlwaysFail]
 
     def do_GET(self):
-        self.send_response(200)
+        try:
+            self.full_path = os.getcwd() + self.path
+
+            for case in self.Cases:
+                handler = case()
+                if handler.test(self):
+                    handler.act(self)
+                    break
+        except Exception as msg:
+            self.handle_error(msg)
+
+    def handle_file(self, path):
+        try:
+            with open(path, 'rb') as reader:
+                content = reader.read()
+                self.send_page(content)
+        except IOError as msg:
+            msg = "'{0}' can't be read: {1}".format(self.path, msg)
+            self.handle_error(msg)
+
+    def handle_error(self, msg):
+        content = self.Error_Page.format(path=self.path, msg=msg)
+        self.send_page(content, status=404)
+
+    def create_page(self):
+        values = {
+            'date_time': self.date_time_string(),
+            'client_host': self.client_address[0],
+            'client_port': self.client_address[1],
+            'command': self.command,
+            'path': self.path,
+        }
+        page = self.Page.format(**values)
+        return page
+
+    def send_page(self, page, status=200):
+        self.send_response(status)
         self.send_header("Content-Type", "text/html")
-        self.send_header("Content-Length", str(len(self.Page)))
+        self.send_header("Content-Length", str(len(page)))
         self.end_headers()
-        self.wfile.write(self.Page)
+        self.wfile.write(page)
 
 
 if __name__ == '__main__':
