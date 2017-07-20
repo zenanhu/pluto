62122fb19dfb4b495fcf06e781032aed60f233a1
Zenan <zenan2048@gmail.com>
2017-07-20 14:28:00 +0800

    fork process to handle more than one request

--- hydra/server1.py
+++ hydra/server1.py
@@ -4,11 +4,12 @@ import time
 
 
 SERVER_ADDRESS = (HOST, PORT) = '', 8888
-REQUEST_QUEUE_SIZE = 1
+REQUEST_QUEUE_SIZE = 5
 
 
 def handle_request(client_connection):
     request = client_connection.recv(1024)
+    print 'Child PID: {pid}. Parent PID: {ppid}'.format(pid=os.getpid(), ppid=os.getppid())
     print request.decode()
     http_response = '''\
 HTTP/1.1 200 OK
@@ -24,12 +25,19 @@ def serve_forever():
     listen_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     listen_socket.bind(SERVER_ADDRESS)
     listen_socket.listen(REQUEST_QUEUE_SIZE)
-    print 'Serving HTTP on port {port} ...\n'.format(port=PORT)
+    print 'Serving HTTP on port {port} ...'.format(port=PORT)
+    print 'Parent PID (PPID): {pid}\n'.format(pid=os.getpid())
 
     while True:
         client_connection, client_address = listen_socket.accept()
-        handle_request(client_connection)
-        client_connection.close()
+        pid = os.fork()
+        if pid == 0:
+            listen_socket.close()
+            handle_request(client_connection)
+            client_connection.close()
+            os._exit(0)
+        else:
+            client_connection.close()
 
 if __name__ == '__main__':
     serve_forever()
